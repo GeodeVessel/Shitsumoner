c1a2ced60ccb899d52ab1e7af8d835e29901b3c0
GeodeVessel <pedsantosandrade@gmail.com>
2024-11-29 21:53:56 -0300

    initial commit

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Japonês',
+    'O que significa "shitsumon"?',
+    'Shitsumon (質問) significa "pergunta" ou "questionamento"'
+)
+
+criaCartao(
+    'Arte digital',
+    'O que é a line art de um desenho?',
+    'É a camada de linhas monocromaticas de um desenho, com diferentes pesos e grossuras, feitas sobre o rascunho inicial'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Uma váriavel é um espaço na memória do computador onde se armazena um dado a ser usado dentro de um código'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um radiano?',
+    'O radiano (rad) é uma unidade de medida que representa a razão entre o comprimento de um arco e o raio da circunferência'
+)
+
+criaCartao(
+    'Música',
+    'O que é um intervalo?',
+    'Um intervalo na música é a distância entre duas notas, sendo um acorde composto por múltiplos intervalos'
+)
+
+criaCartao(
+    'Língua inglesa',
+    'O que significa a expressão "Hook, line and sinker"?',
+    'A expressão é utilizada quando alguém é completamente enganado ou preso em uma situação sem escapatória'
+)
